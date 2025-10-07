5ec62fc09ae1f279ea37c11315dca59f295b2b0e
filipvuk023-bit <filip.vuk023@gmail.com>
2025-10-07 18:48:07 +0200

    Initial commit

---
+++ Tocka.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Tocka
+{
+    public double X;
+    public double Y;
+
+    public Tocka()
+    {
+        this.X = 0;
+        this.Y = 0;
+        Console.WriteLine("Default");
+    }
+    public Tocka(double x, double y)
+    {
+    this.X = x;
+    this.Y = y;
+    }
+}
---
+++ Trokut.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Trokut
+{
+    public Tocka A;
+    public Tocka B;
+    public Tocka C;
+    public Tocka D;
+    public Trokut()
+    {
+        Console.WriteLine("Ovo je konstrukcija trokuta");
+    }
+    public double Povrsina()
+    {
+        return 0;
+    }
+}
